a375fb647888e75391e78b65bc859f700aa3156c
Geeoon Chung <geeoon13@gmail.com>
2022-03-09 15:45:39 -0600

    Updated MeepMeep during competition.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,42 +13,48 @@ public class MeepMeepTesting {
 		MeepMeep meepMeep = new MeepMeep(800);
 
 		RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-				.setStartPose(new Pose2d(-36.0, 62.0, -Math.PI / 2.0))
+				.setStartPose(new Pose2d(12.0, 62.0, -Math.PI / 2.0))
 				// Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
 				.setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
 
 				.followTrajectorySequence(drive ->
 						drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+								//.setVelConstraint(drivetrain.getDrivetrain().getVelocityConstraint(10.0, MAX_ANG_VEL, TRACK_WIDTH))
 								.lineToConstantHeading(new Vector2d(-14.0, 62.0))  // go in front of the shipping hub
+								.resetVelConstraint()
 								.addTemporalMarker(() -> {
-									//arm.setPosition(.62);
-									//slide.setPosition(1.32);
+									//arm.setPosition(1.2);
+									//wrist.setPosition(0.36);
 								})
 								.waitSeconds(5)
-								.lineToConstantHeading(new Vector2d(-14.0, 48.0))  // go in front of the shipping hub
+								.lineToConstantHeading(new Vector2d(-14.0, 43.0))  // go in front of the shipping hub
 								.addTemporalMarker(() -> {
 									//intake.setPosition(0.0);
 								})
 								.waitSeconds(0.5)
-								.lineToConstantHeading(new Vector2d(-62.5, 56.5))  // go to the carousel thing
-								.UNSTABLE_addTemporalMarkerOffset(-0.5, () -> {
-									//carousel.setSpeed(0.5);
+								.lineToConstantHeading(new Vector2d(-14.0, 53.5))
+								.addTemporalMarker(() -> {
+									//arm.setPosition(.67);
+									//wrist.setPosition(0.0);
+									//intake.setPosition(1.0);
 								})
+								.waitSeconds(2)
+								.addTemporalMarker(() -> {
+									//arm.kill();
+									//wrist.kill();
+								})
+								.lineToLinearHeading(new Pose2d(-14.00, 59.0, Math.PI))  // Rotate
+								.UNSTABLE_addTemporalMarkerOffset(-0.5, () -> {
+									//carousel.setSpeed(-0.5);
+								})
+								//.setVelConstraint(drivetrain.getDrivetrain().getVelocityConstraint(10.0, MAX_ANG_VEL, TRACK_WIDTH))
 								.waitSeconds(3.5)
 								.addTemporalMarker(() -> {
 									//carousel.setSpeed(0.0);
 								})
+								.lineToConstantHeading(new Vector2d(13.0, 63.5))  // Line up to park
 								//.setVelConstraint(drivetrain.getDrivetrain().getVelocityConstraint(10.0, MAX_ANG_VEL, TRACK_WIDTH))
-								.lineToConstantHeading(new Vector2d(-60, 34))  // park
-								.addTemporalMarker(() -> {
-									//arm.setPosition(2);
-									//slide.setPosition(.5);
-								})
-								.waitSeconds(2)
-								.addTemporalMarker(() -> {
-								//	arm.kill();
-								//	slide.kill();
-								})
+								.lineToConstantHeading(new Vector2d(32, 65))
 								.resetVelConstraint()
 								.build()
 				);
